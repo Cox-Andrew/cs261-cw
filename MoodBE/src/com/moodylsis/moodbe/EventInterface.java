b3a177b659129a550691e146f5cad8d41421dded
tpeach90 <thomasopeach@btinternet.com>
2021-02-22 14:28:17 +0000

    added "throws SQLException" to everything

--- MoodBE/src/com/moodylsis/moodbe/EventInterface.java
+++ MoodBE/src/com/moodylsis/moodbe/EventInterface.java
@@ -1,6 +1,7 @@
 package com.moodylsis.moodbe;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Date;
 
 import javax.servlet.http.HttpServletResponse;
@@ -8,26 +9,26 @@ import javax.servlet.http.HttpServletResponse;
 public interface EventInterface {
 	
 	// GET /v0/events/{eventID}
-	public String getJSON(Connection conn, int eventID);
+	public String getJSON(Connection conn, int eventID) throws SQLException;
 	
 	// GET /v0/events/{eventID}/invite-code
-	public String getInviteCode(Connection conn, int eventID);
+	public String getInviteCode(Connection conn, int eventID) throws SQLException;
 	
 	// GET /v0/event/{eventID}/analytics
 	// don't know what this will return yet :(
-	public String getAnalytics(Connection conn, int eventID);
+	public String getAnalytics(Connection conn, int eventID) throws SQLException;
 	
 	// POST /v0/events
 	/* Should return the ID of the new event, or null if not created */
-	public int newEvent(Connection conn, int seriesID, String title, String description, Date start, Date end);
+	public int newEvent(Connection conn, int seriesID, String title, String description, Date start, Date end) throws SQLException;
 	
 	// POST /v0/events/{eventID}/forms
-	public boolean newEventForm(Connection conn, int eventID, int formID, int preceedingFormID, Date timeStart, Date timeEnd);
+	public boolean newEventForm(Connection conn, int eventID, int formID, int preceedingFormID, Date timeStart, Date timeEnd) throws SQLException;
 	
 	// PUT /v0/events/{eventID}
-	public boolean editEvent(Connection conn, int EventID, int seriesID, String newTitle, String newDescription, Date newStart, Date newEnd);
+	public boolean editEvent(Connection conn, int EventID, int seriesID, String newTitle, String newDescription, Date newStart, Date newEnd) throws SQLException;
 	
 	// DELETE /v0/events/{eventID}
-	public boolean deleteEvent(Connection conn, int eventID);
+	public boolean deleteEvent(Connection conn, int eventID) throws SQLException;
 	
 }
--- MoodBE/src/com/moodylsis/moodbe/SeriesInterface.java
+++ MoodBE/src/com/moodylsis/moodbe/SeriesInterface.java
@@ -1,20 +1,21 @@
 package com.moodylsis.moodbe;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public interface SeriesInterface {
 	// GET /v0/series/{seriesID}
-	public String getJSON(Connection conn, int seriesID);
+	public String getJSON(Connection conn, int seriesID) throws SQLException;
 	
 	// POST /v0/series
 	// I didn't put events:list in because the series should be created before the events
-	public int newSeries(Connection conn, String title, String desc, int hostID);
+	public int newSeries(Connection conn, String title, String desc, int hostID) throws SQLException;
 	
 	// PUT /v0/series/{seriesID}
-	public boolean newSeries(Connection conn, int seriesID, String newTitle, String newDesc);
+	public boolean newSeries(Connection conn, int seriesID, String newTitle, String newDesc) throws SQLException;
 	
 	// DELETE /v0/series/{seriesID}
-	public boolean deleteSeries(Connection conn, int seriesID);
+	public boolean deleteSeries(Connection conn, int seriesID) throws SQLException;
 	
 	
 	// there are other methods in the class diagram but I'm not sure if we need all of them, we can add them later if we do
