e5c1933adb4d5843d3868c821e46c18de56d3c7e
tpeach90 <thomasopeach@btinternet.com>
2021-02-23 14:42:18 +0000

    different way of doing endpoints test

---
+++ MoodBE/src/com/moodylsis/moodbe/endpoints/EventSingleEndpoint.java
@@ -0,0 +1,20 @@
+package com.moodylsis.moodbe.endpoints;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+
+@Path("/v0/events/{eventID}")
+public class EventSingleEndpoint {
+	
+	@GET
+	@Produces("application/json")
+	public String getEventRequest(@PathParam("eventID") String eventIDString) {
+		
+		
+		
+		return null;
+	}
+}
