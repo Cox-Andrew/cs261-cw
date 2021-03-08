042b1f0449d9b39f7b7a445e839cc7da732c8530
tpeach90 <thomasopeach@btinternet.com>
2021-03-08 13:55:25 +0000

    host test updated - probably overkill

--- tests/testing_tools.py
+++ tests/testing_tools.py
@@ -10,6 +10,8 @@ import random
 
 base = "http://localhost:8001/v0/"
 conn = psycopg2.connect(database="mood", user="mooduser", password="password", host="localhost", port="8432")
+conn.autocommit = True
+
 
 def cursor():
 	return conn.cursor(cursor_factory = psycopg2.extras.DictCursor)
@@ -24,6 +26,15 @@ def getObjectFromDB(table, id):
 		cur.execute(f"SELECT * FROM {table} where {depluralized}id = {str(id)}")
 		out = cur.fetchone()
 	return out
+
+def deleteObjectFromDB(table, id):
+	depluralized = table
+	if table[-1] == "s" and table != "events":
+		depluralized = table[:-1]
+
+	with cursor() as cur:
+		cur.execute(f"DELETE FROM {table} where {depluralized}id = {str(id)}")
+
 
 def existsInDB(table, id):
 	depluralized = table
