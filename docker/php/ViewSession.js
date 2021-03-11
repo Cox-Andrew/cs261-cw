f3fadd7ab326d5f496debaf2f8d5ee405807fd58
PrithuC <78407145+PrithuC@users.noreply.github.com>
2021-03-11 13:53:11 +0000

    Update ViewSession.js

--- docker/php/ViewSession.js
+++ docker/php/ViewSession.js
@@ -28,6 +28,58 @@ function showDiv() {
   document.getElementById("msg").style.display= " block";
 }
 
+//general tab
+google.charts.load("current", {packages:["corechart"]});
+google.charts.setOnLoadCallback(drawChart1);
+function drawChart1() {
+  // example values
+  var Excellent = 10; //assign excellent to a mood value from backend
+  var VGood = 5;
+  var Good = 4;
+  var NGood = 3;
+  var Bad = 1;
+
+  var data = google.visualization.arrayToDataTable([
+    ['Mood', 'Mood value'],
+    ['Excellent', Excellent],
+    ['Very good', VGood],
+    ['Good', Good],
+    ['Not good', NGood],
+    ['Bad', Bad]
+  ]);
+
+  var options = {
+    title: 'Mood analysis',
+    is3D: true,
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+  chart.draw(data, options);
+}
+
+//comprehensive tab
+google.charts.load("current", {packages:["corechart"]});
+google.charts.setOnLoadCallback(drawChart2);
+function drawChart2() {
+  //example values
+  var q1 = 10; //assign q1 to a mood value from backend
+  var q2 = 5;
+  var q3 = 2;
+  var data = google.visualization.arrayToDataTable([
+    ['Question', 'Mood value'],
+    ['Q1', q1],
+    ['Q2', q2],
+    ['Q3', q3]
+  ]);
+
+  var options = {
+    title: 'Mood analysis',
+    pieHole: 0.4,
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+  chart.draw(data, options);
+}
 
 
 
