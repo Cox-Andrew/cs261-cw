7648c4abde5b4ee60e41e625d08d49f0f26dcfaa
Jude Smithers <jude@kitsnet.co.uk>
2021-03-11 14:01:11 +0000

    join meeting implemented, still need to have anonymity??

--- docker/php/JoinMeeting.js
+++ docker/php/JoinMeeting.js
@@ -1,13 +1,33 @@
 function submitForm() {
-    const mname = document.getElementById('mname').value;
-    const name = document.getElementById('name').value;
-    var checkbox = document.getElementById('anonymous');
+  const inviteCode = document.getElementById('mname').value;
+  var checkbox = document.getElementById('anonymous');
 
-    var anonymous = 0;
-    if (checkbox.checked == true){
-        anonymous = 1;   //anonymous is 1 when selected and 0 otherwise
+  var anonymous = 0;
+  if (checkbox.checked == true){
+      anonymous = 1;   //anonymous is 1 when selected and 0 otherwise
+  }
+
+  // Call backend function here
+  // On success, Redirect to "AttendeePage.html"
+  joinMeeting(inviteCode);
+}
+
+function joinMeeting(inviteCode) {
+  register = {};
+  register["invite-code"] = inviteCode;
+  register["attendeeID"] = getCookie("attendeeID");
+
+  $.ajax({
+    type: "POST",
+    url: endpointToRealAddress("/register-event"),
+    dataType: "json",
+    contentType: "application/json",
+    data: JSON.stringify(register),
+    async: false,
+    success: function(result, status, xhr){
+      eventID = result.eventID;
+      setCookie("eventID", eventID, 1);
+      window.location.href = "/AttendeePage.html";
     }
-
-    // Call backend function here
-    // On success, Redirect to "AttendeePage.html"
+  });
 }
