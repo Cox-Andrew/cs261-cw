a7bce70f7352827c5437da3b0dc28a994a0078d5
tpeach90 <thomasopeach@btinternet.com>
2021-03-12 12:04:43 +0000

    half-finished manage meeting

--- docker/php/ManageMeeting.js
+++ docker/php/ManageMeeting.js
@@ -43,3 +43,168 @@ function submitTemplate2() {
 
   // Call backend function here
 }
+
+
+
+
+
+
+// the large container div elements
+var pageForms = [];
+var pageBubbles = [];
+
+
+
+
+// create a place for a form to be displayed
+function blankFormFactory() {
+
+  var formhtml = `<div class = "template1">
+    <div class = "form">
+      <h3 class="template-title"></h3>
+      <h4 class="template-description"></h4>
+      <form id = "temp1" >
+        <input type = "submit" class = "submitTemp1" value="Save"></input>
+      </form>
+    </div>
+  </div>
+  `
+  var wrapper = document.createElement("div");
+  wrapper.innerHTML = formhtml;
+  var newNode = wrapper.firstChild;
+  return newNode;
+}
+
+// create a place for a question to be displayed
+function blankQuestionFactory(hasOptions=false) {
+  
+  var questionhtml = `<label></label><br><textarea class = "textarea" style='font-size: 16px; font-family: "poppins", sans-serif' rows="2" cols="15"></textarea>`;
+  if (hasOptions) questionhtml += '<div class="options"></div>';
+
+  var wrapper = document.createElement("div");
+  wrapper.innerHTML = formhtml;
+  return wrapper;
+}
+
+function blankBubbleFactory() {
+
+}
+
+// create a new form in the browser (not in db yet)
+function addNewDisplayForm() {
+
+  displayFormNumber = 0;
+  return displayFormNumber;
+}
+
+// create a new question in the browser (not in db yet)
+function addNewDisplayQuestion(displayFormNumber) {
+
+
+  displayQuestionNumber = 0;
+  return displayQuestionNumber;
+}
+
+// submits the newly created form to the backend
+function submitDisplayForm(displayFormNumber) {
+
+
+}
+
+
+function createAndDisplayForm(eventFormID, formID, form) {
+
+  // make form title and description
+  var templateNode = blankFormFactory();
+  var formNode = templateNode.getElementsByClassName("form")[0];
+  setInnerHTMLSanitized(formNode.getElementsByClassName("template-title")[0], form.data.title);
+  setInnerHTMLSanitized(formNode.getElementsByClassName("template-description")[0], form.data.description);
+
+  // create questions
+  for (const i in form.questions) {
+    var question = form.questions[i];
+    var questionID = form.questionIDs[i];
+    createQuestion(question, questionID, formNode, i+1);
+  }
+
+  // add a bubble at the top of the screen
+
+
+  // add the form to the page
+
+
+  // set up page events to switch between bubbles
+
+
+  
+
+  return newNode;
+}
+
+function createQuestion(questionID, question, appendTo, questionDisplayNumber) {
+
+  var questionWrapper = blankQuestionFactory();
+
+  // add text like "Question 1 - multi" in the <label>
+  setInnerHTMLSanitized(questionWrapper.getElementsByTagName("label")[0], "Question " + questionDisplayNumber + " - " + question.data.type);
+
+  // insert the current data
+  setInnerHTMLSanitized(questionWrapper.getElementsByTagName("textarea")[0], question.data.text);
+
+  // add options
+  if (question.data.type == "multi") {
+    question.data.options.forEach(option => {
+
+      // create box to contain the output 
+      var optionWrapper = document.createElement("div");
+      optionWrapper.innerHTML = `<textarea class = "textarea" style='font-size: 16px; font-family: "poppins", sans-serif' rows="1" cols="15">`;
+      var optionBox = optionWrapper.childNodes[0];
+
+      // add option box to the form
+      setInnerHTMLSanitized(optionBox, option);
+      questionWrapper.getElementsByClassName("options")[0].appendChild(optionBox);
+    });
+  }
+
+  // append the question to the form
+  questionWrapper.childNodes.forEach(questionPart => {
+    appendTo.appendChild(questionPart);
+  });
+
+
+
+  
+}
+
+
+
+
+// get eventID
+var eventIDString = getCookie("eventID");
+if (eventIDString == "") {
+  window.location.href = "/MeetingList.html"
+}
+var eventID = parseInt(eventIDString);
+
+
+// The following items are added to eventData:
+// same as the form in formData, but with null for some formIDs that haven't been submitted yet
+// and also with "displayFormNumber" field.
+// the nested questions also have these two properties, i.e. "displayQuestionNumber"
+
+var eventData;
+
+getAllEventData(eventID, function(e_d) {
+  eventData = e_d;
+
+  for (i=0; i<eventData.eventFormIDs.length; i++) {
+
+    var eventFormID = eventData.eventFormIDs[i];
+    var formID = eventData.formIDs[i];
+    var form = eventData.forms[i];
+
+    createAndDisplayForm(eventFormID, formID, form);
+  }
+});
+
+
