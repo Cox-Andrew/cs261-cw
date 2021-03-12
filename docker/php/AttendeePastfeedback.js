3cb68d7cfe3705d67cbd466b5cb672a32aae2ee6
PrithuC <78407145+PrithuC@users.noreply.github.com>
2021-03-12 13:02:30 +0000

    Add files via upload

---
+++ docker/php/AttendeePastfeedback.js
@@ -0,0 +1,27 @@
+var gen = document.getElementById("general");
+var feed = document.getElementById("feedback");
+function general() {
+  if (gen.style.display === "none" || gen.style.display === '')
+  {
+    if(feed.style.display !== "none" || feed.style.display !== '')
+    {
+      feed.style.display = "none";
+    }
+    gen.style.display = "block";
+  } else {
+    gen.style.display = "none";
+  }
+}
+function feedback() {
+  if (feed.style.display === "none" || feed.style.display === '')
+  {
+    if(gen.style.display !== "none" || gen.style.display !== '')
+    {
+      gen.style.display = "none";
+    }    
+    feed.style.display = "block"; 
+  } else {
+    feed.style.display = "none";
+  }
+}
+
