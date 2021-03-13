ff28ebe85e1c1a0f8754d39a96d23b8714ffd4de
tpeach90 <thomasopeach@btinternet.com>
2021-03-13 19:54:04 +0000

    time fixed

--- MoodBE/src/com/moodlysis/moodbe/ServerTimeRequest.java
+++ MoodBE/src/com/moodlysis/moodbe/ServerTimeRequest.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONObject;
+
 
 @WebServlet("/v0/server-time")
 public class ServerTimeRequest extends HttpServlet {
@@ -19,9 +21,13 @@ public class ServerTimeRequest extends HttpServlet {
     }
 
 
+	@SuppressWarnings("unchecked")
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().print(LocalDateTime.now().toString());
+		response.setContentType("application/json");
+		JSONObject obj = new JSONObject();
+		obj.put("server-time", LocalDateTime.now().toString());
+		response.getWriter().print(obj.toJSONString());
 	}
 
 
