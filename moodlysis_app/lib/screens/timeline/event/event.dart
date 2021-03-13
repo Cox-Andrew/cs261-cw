711583e265322f4dff5d4a01dbc63d9a1bfe9aff
Andrew Cox <18695702+Cox-Andrew@users.noreply.github.com>
2021-03-13 21:08:32 +0000

    setup event page framework

--- moodlysis_app/lib/screens/timeline/event/event.dart
+++ moodlysis_app/lib/screens/timeline/event/event.dart
@@ -1,28 +1,68 @@
 import 'package:flutter/material.dart';
 
 import 'package:moodlysis_app/screens/timeline/event/arguments.dart';
+import 'package:moodlysis_app/utils/events.dart';
 
 class EventScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final EventScreenArgs args = ModalRoute.of(context).settings.arguments;
+    bool _live = args.event.schedule.start.isBefore(DateTime.now());
 
     return Scaffold(
       appBar: AppBar(
         title: Text("Event"),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(20),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 10),
+              child: RichText(
+                text: TextSpan(children: [
+                  TextSpan(
+                      text: 'Welcome to ',
+                      style: TextStyle(fontWeight: FontWeight.normal)),
+                  TextSpan(
+                      text: args.event.title,
+                      style: TextStyle(color: Theme.of(context).primaryColor)),
+                  TextSpan(
+                      text: '!',
+                      style: TextStyle(fontWeight: FontWeight.normal)),
+                ], style: Theme.of(context).textTheme.headline4),
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
             Text(
-              "Hey! Welcome to ${args.event.title}",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+              args.event.description,
+              style: Theme.of(context).textTheme.subtitle1,
+              maxLines: 3,
+              overflow: TextOverflow.ellipsis,
             ),
-            SizedBox(
-              height: 10,
-            ),
-            FeedbackForm(),
+            Divider(),
+            Row(children: [
+              Icon(Icons.calendar_today,
+                  size: Theme.of(context).textTheme.subtitle1.fontSize),
+              Text(' ${args.event.schedule.humanReadable}',
+                  style: Theme.of(context)
+                      .textTheme
+                      .subtitle1
+                      .copyWith(color: Theme.of(context).errorColor)),
+            ]),
+            Divider(),
+            _live
+                ? Column(children: [
+                    GeneralFeedbackForm(),
+                    Divider(),
+                    ComprehensiveFeedbackForm(),
+                  ])
+                : Expanded(
+                    child: Align(
+                        alignment: FractionalOffset(0.5, 0.3),
+                        child: Text("This event hasn't started yet.", style: Theme.of(context).textTheme.headline3, textAlign: TextAlign.center,))),
           ],
         ),
       ),
@@ -30,64 +70,104 @@ class EventScreen extends StatelessWidget {
   }
 }
 
-class FeedbackForm extends StatefulWidget {
+class ComprehensiveFeedbackForm extends StatefulWidget {
   @override
-  FeedbackFormState createState() {
-    return FeedbackFormState();
+  ComprehensiveFeedbackFormState createState() {
+    return ComprehensiveFeedbackFormState();
   }
 }
 
-class FeedbackFormState extends State<FeedbackForm> {
-  // Create a global key that uniquely identifies the Form widget
-  // and allows validation of the form.
+class ComprehensiveFeedbackFormState extends State<ComprehensiveFeedbackForm> {
   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Form(
         key: _formKey,
-        child: Column(children: <Widget>[
-          Column(
-            children: [
-              Text(
-                  "This is a short question This is a short question This is a short question This is a short question"),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: "Response",
-                  border: OutlineInputBorder(),
-                ),
-                keyboardType: TextInputType.text,
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Required';
-                  }
-                  return null;
-                },
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text("Comprehensive Feedback",
+                  style: Theme.of(context).textTheme.headline5),
+              Column(
+                children: [
+                  Text(
+                      "This is a short question This is a short question This is a short question This is a short question"),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: "Response",
+                      border: OutlineInputBorder(),
+                    ),
+                    keyboardType: TextInputType.text,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Required';
+                      }
+                      return null;
+                    },
+                  ),
+                ],
               ),
-            ],
-          ),
-          TextFormField(
-            decoration: const InputDecoration(
-              labelText: "Response",
-            ),
-            maxLines: 3,
-            keyboardType: TextInputType.multiline,
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Required';
-              }
-              return null;
-            },
-          ),
-          ElevatedButton(
-            onPressed: () {
-              if (_formKey.currentState.validate()) {
-                Scaffold.of(context)
-                    .showSnackBar(SnackBar(content: Text('Processing Data')));
-              }
-            },
-            child: Text('Submit'),
-          ),
-        ]));
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    Scaffold.of(context).showSnackBar(
+                        SnackBar(content: Text('Processing Data')));
+                  }
+                },
+                child: Text('Submit'),
+              ),
+            ]));
+  }
+}
+
+class GeneralFeedbackForm extends StatefulWidget {
+  @override
+  GeneralFeedbackFormState createState() {
+    return GeneralFeedbackFormState();
+  }
+}
+
+class GeneralFeedbackFormState extends State<GeneralFeedbackForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text("General feedback",
+                  style: Theme.of(context).textTheme.headline5),
+              Column(
+                children: [
+                  Text(
+                      "This is a short question This is a short question This is a short question This is a short question"),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: "Response",
+                      border: OutlineInputBorder(),
+                    ),
+                    keyboardType: TextInputType.text,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Required';
+                      }
+                      return null;
+                    },
+                  ),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    Scaffold.of(context).showSnackBar(
+                        SnackBar(content: Text('Processing Data')));
+                  }
+                },
+                child: Text('Submit'),
+              ),
+            ]));
   }
 }
