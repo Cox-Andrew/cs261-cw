ec9759a37e2bdafd1116a44df0d478797ac10f2e
Andrew Cox <18695702+Cox-Andrew@users.noreply.github.com>
2021-03-14 12:08:31 +0000

    added Sentiment constants

--- moodlysis_app/lib/constants.dart
+++ moodlysis_app/lib/constants.dart
@@ -9,3 +9,19 @@ const String timelineScreenRoute = '/timeline';
 const String eventScreenRoute = '/timeline/event';
 const String registerEventScreenRoute = '/register_event';
 const String accountScreenRoute = '/account';
+
+enum Sentiment {
+  veryDissatisfied,
+  dissatisfied,
+  neutral,
+  satisfied,
+  verySatisfied
+}
+
+final Map<Sentiment, double> sentimentValues =  {
+  Sentiment.veryDissatisfied: -1,
+  Sentiment.dissatisfied: -0.5,
+  Sentiment.neutral: 0,
+  Sentiment.satisfied: 0.5,
+  Sentiment.verySatisfied: 1,
+};
