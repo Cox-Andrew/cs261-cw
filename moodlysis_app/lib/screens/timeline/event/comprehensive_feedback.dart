a7a865fea30f16674e30f1afee92069d08214fe7
Andrew Cox <18695702+Cox-Andrew@users.noreply.github.com>
2021-03-14 16:47:01 +0000

    test fetch form data (working!)

--- moodlysis_app/lib/screens/timeline/event/comprehensive_feedback.dart
+++ moodlysis_app/lib/screens/timeline/event/comprehensive_feedback.dart
@@ -1,4 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:moodlysis_app/models/form.dart';
+import 'package:moodlysis_app/models/question.dart';
+import 'package:moodlysis_app/services/forms.dart';
+import 'package:moodlysis_app/globals.dart' as globals;
+import 'package:moodlysis_app/utils/error_handlers.dart';
 
 class ComprehensiveFeedbackForm extends StatefulWidget {
   @override
@@ -9,38 +18,27 @@ class ComprehensiveFeedbackForm extends StatefulWidget {
 
 class ComprehensiveFeedbackFormState extends State<ComprehensiveFeedbackForm> {
   final _formKey = GlobalKey<FormState>();
+  EventForm _activeForm;
+
+  @override
+  void initState() {
+    getActiveForm(http.Client(), globals.currentEvent)
+        .then((form) => setState(() => _activeForm = form))
+        .catchError((e, s) => handleConnectionError(context, e, s),
+            test: (e) => e is SocketException)
+        .catchError((e, s) => handleError(context, e, s));
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 8.0),
-            child: Text("Comprehensive feedback",
-                style: Theme.of(context).textTheme.headline5),
-          ),
-          Divider(),
-          Column(
-            children: [
-              Text(
-                  "This is a short question This is a short question This is a short question This is a short question"),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: "Response",
-                  border: OutlineInputBorder(),
-                ),
-              ),
-            ],
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text('Submit'),
-          ),
-        ],
-      ),
-    );
+        key: _formKey,
+        child: _activeForm == null
+            ? Text("The host has not made any comprehensive forms available at this moment")
+            : Column(
+                children: _activeForm.questions
+                    .map((Question q) => Text('${q.text} (${q.type})'))
+                    .toList()));
   }
 }
