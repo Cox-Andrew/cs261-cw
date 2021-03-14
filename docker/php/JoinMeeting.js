6cdd6c8dc68313ce7a597734bf227d39d89b2605
Jude Smithers <jude@kitsnet.co.uk>
2021-03-14 15:17:33 +0000

    fixed anon checkbox eventjoin, also made primary key attendeeid, eventid so you cannot register for an event twice (error is not handled in frontend)

--- docker/php/JoinMeeting.js
+++ docker/php/JoinMeeting.js
@@ -1,11 +1,11 @@
 function submitForm() {
   const inviteCode = document.getElementById('mname').value;
-  var checkbox = document.getElementById('anonymous');
+  /*var checkbox = document.getElementById('anonymous');
 
   var anonymous = 0;
   if (checkbox.checked == true){
       anonymous = 1;   //anonymous is 1 when selected and 0 otherwise
-  }
+  }*/
 
   // Call backend function here
   // On success, Redirect to "AttendeePage.html"
